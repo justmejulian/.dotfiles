fc022e3f89aa57ce713cd94f4d6f12612ec803c6
Julian Visser <chulio.visser@gmail.com>
2022-05-07 15:50:03 +0200

    removed checks from script

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,14 +1,8 @@
+# https://www.atlassian.com/git/tutorials/dotfiles
+
 git clone --bare git@github.com:justmejulian/.dotfiles.git $HOME/.dotfiles
 function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
 config checkout
 config config status.showUntrackedFiles no
