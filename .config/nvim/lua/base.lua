7a888be2b147cf8c79c3ecbeb22a7976d2838125
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2022-09-02 12:21:50 +0200

    added lua config following https://dev.to/craftzdog/my-neovim-setup-for-react-typescript-tailwind-css-etc-58fb

---
+++ .config/nvim/lua/base.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = " "
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.showcmd = true
+vim.opt.expandtab = true
+vim.opt.laststatus = 2
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.mouse = vim.opt.mouse + 'a'
