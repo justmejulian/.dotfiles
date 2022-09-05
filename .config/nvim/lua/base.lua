9734dca978c1e2080c73bf7b4fab56a215cea05b
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2022-09-05 07:49:41 +0200

    setup base config

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -3,12 +3,29 @@ vim.g.mapleader = " "
 vim.wo.number = true
 vim.wo.relativenumber = true
 
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.showcmd = true
-vim.opt.expandtab = true
 vim.opt.laststatus = 2
-vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+
 vim.opt.mouse = vim.opt.mouse + 'a'
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.list = true
+--[[
+vim.opt.listchars = {
+    nbsp = '␣',
+    trail = '•',
+    eol = '↵',
+    tab = '--→',
+}
+]]--
+vim.opt.listchars = {
+    nbsp = '␣',
+    trail = '•',
+    tab = '--→',
+}
