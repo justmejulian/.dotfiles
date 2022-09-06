13bed8b5088200e61263471f37a8c21fe3641082
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2022-09-06 11:45:15 +0200

    Update install.sh
    do inital load via ssh

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,6 +1,6 @@
 # https://www.atlassian.com/git/tutorials/dotfiles
 
-git clone --bare git@github.com:justmejulian/.dotfiles.git $HOME/.dotfiles
+git clone --bare https://github.com/justmejulian/.dotfiles.git $HOME/.dotfiles
 function config {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
