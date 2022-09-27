71b27015e012b7e6f9df99751b2f82a59c9980b2
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2022-09-27 18:52:08 +0200

    added why to delete buffer

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,41 +4,50 @@ local actions = require('telescope.actions')
 local builtin = require("telescope.builtin")
 
 local function telescope_buffer_dir()
-  return vim.fn.expand('%:p:h')
+    return vim.fn.expand('%:p:h')
 end
 
-telescope.setup {}
+telescope.setup{
+    defaults = {
+        -- Default configuration for telescope goes here:
+        -- config_key = value,
+        mappings = {
+            n = {
+                ['<c-d>'] = require('telescope.actions').delete_buffer
+            },
+        }
+    },
+}
 
 telescope.load_extension("file_browser")
 vim.keymap.set('n', '<Leader>ff',
-  function()
-    builtin.find_files({
-      no_ignore = false,
-      hidden = true,
-      file_ignore_patterns = { "node_modules", ".git"}
-    })
-  end, { desc = "telescope find_files" })
+    function()
+        builtin.find_files({
+            no_ignore = false,
+            hidden = true,
+            file_ignore_patterns = { "node_modules", ".git" }
+        })
+    end, { desc = "telescope find_files" })
 vim.keymap.set('n', '<Leader>fg', function()
-  builtin.live_grep()
+    builtin.live_grep()
 end, { desc = "telescope live_grep" })
 vim.keymap.set('n', '<Leader>fb', function()
-  builtin.buffers()
+    builtin.buffers()
 end, { desc = "telescope buffers" })
 vim.keymap.set('n', '<Leader>fh', function()
-  builtin.help_tags()
+    builtin.help_tags()
 end, { desc = "telescope help_tags" })
 vim.keymap.set('n', '<Leader>fd', function()
-  builtin.diagnostics()
+    builtin.diagnostics()
 end, { desc = "telescope diagnostics" })
 vim.keymap.set("n", "<Leader>fe", function()
-  telescope.extensions.file_browser.file_browser({
-    path = "%:p:h",
-    cwd = telescope_buffer_dir(),
-    respect_gitignore = false,
-    hidden = true,
-    grouped = true,
-    initial_mode = "normal",
-    layout_config = { height = 40 }
-  })
+    telescope.extensions.file_browser.file_browser({
+        path = "%:p:h",
+        cwd = telescope_buffer_dir(),
+        respect_gitignore = false,
+        hidden = true,
+        grouped = true,
+        initial_mode = "normal",
+        layout_config = { height = 40 }
+    })
 end, { desc = "telescope file_browser" })
-
