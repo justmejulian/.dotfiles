2165c074a792b3ad7b1eba07d73e84e57568f820
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2022-12-23 10:28:29 +0100

    add shortcurt for git signs

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -1,1 +1,14 @@
-require('gitsigns').setup {}
+local status, gitsigns = pcall(require, "gitsigns")
+if (not status) then return end
+
+gitsigns.setup {
+  on_attach = function(bufnr)
+    local function map(mode, lhs, rhs, opts)
+      opts = vim.tbl_extend('force', { noremap = true, silent = true }, opts or {})
+      vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+    end
+
+    -- Actions
+    map('n', '<leader>gb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+  end
+}
