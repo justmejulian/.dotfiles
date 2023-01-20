5539e9c848bce84cc874eb45447dcfe4bdcf53c6
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2023-01-20 11:33:33 +0100

    change diffview to 4 split

--- .config/nvim/after/plugin/diffview.lua
+++ .config/nvim/after/plugin/diffview.lua
@@ -1,7 +1,15 @@
 local status, diffview = pcall(require, "diffview")
 if (not status) then return end
 
-diffview.setup()
+diffview.setup({
+  view = {
+    merge_tool = {
+      -- Config for conflicted files in diff views during a merge or rebase.
+      layout = "diff4_mixed",
+      disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
+    },
+  },
+})
 
 vim.keymap.set('n', '<Leader>gdo', '<cmd>DiffviewOpen<CR>')
 vim.keymap.set('n', '<Leader>gdc', '<cmd>DiffviewClose<CR>')
