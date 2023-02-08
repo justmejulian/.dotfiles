b3dbf7a2b38637dbe03c47125bc41ec8edf67b94
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2023-02-08 13:00:39 +0100

    replace buffer on go to file

--- .config/nvim/after/plugin/diffview.lua
+++ .config/nvim/after/plugin/diffview.lua
@@ -1,12 +1,26 @@
 local status, diffview = pcall(require, "diffview")
 if (not status) then return end
 
+local actions = require("diffview.actions")
+
 diffview.setup({
   view = {
     merge_tool = {
       -- Config for conflicted files in diff views during a merge or rebase.
       layout = "diff4_mixed",
       disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
+    },
+  },
+  keymaps = {
+    disable_defaults = false, -- Disable the default keymaps
+    view = {
+      { "n", "gf", actions.goto_file_edit, { desc = "Open the file in the previous tabpage" } },
+    },
+    file_panel = {
+      { "n", "gf", actions.goto_file_edit, { desc = "Open the file in the previous tabpage" } },
+    },
+    file_history_panel = {
+      { "n", "gf", actions.goto_file_edit, { desc = "Open the file in the previous tabpage" } },
     },
   },
 })
