36afcbbc49d36a5eb892a84ccf1555c952bb3385
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2024-06-20 14:24:55 +0200

    added alias to set tab title for project

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,7 +6,29 @@ alias ll='exa -la'
 alias lg='exa -la --git'
 alias grep="grep --color='auto'"
 alias z="zellij"
-cdl () { cd "${@}" ; ls ;}
+cdl() { cd "${@}" ; ls ;}
 
 alias nd="nvim -c 'DiffviewOpen' ."
 alias ndm="nvim -c 'DiffviewOpen origin/master' ."
+
+# edit project
+ep() {
+  foldername=$(basename $(pwd))
+  inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+
+  if [ "$inside_git_repo" ]; then
+    gitroot=$(basename $(git rev-parse --show-toplevel))
+
+
+    if [[ $foldername == $gitroot ]]; then
+      kitten @ set-tab-title $foldername
+      nvim .
+    else
+      kitten @ set-tab-title $gitroot - $foldername
+      nvim .
+    fi
+  else
+    kitten @ set-tab-title $foldername
+    nvim .
+  fi
+}
