40dc8e1729a26e98962a8fcb20a4078ede3d1edf
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-01-19 15:17:22 +0100

    improve bink config

--- .config/nvim/lua/config/plugins/blink.lua
+++ .config/nvim/lua/config/plugins/blink.lua
@@ -3,7 +3,33 @@ return {
   dependencies = 'rafamadriz/friendly-snippets',
   version = 'v0.*',
   opts = {
-    keymap = { preset = 'enter' },
+    keymap = {
+      preset = 'none',
+
+      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<C-e>'] = { 'hide', 'fallback' },
+      ['<CR>'] = { 'accept', 'fallback' },
+
+      ['<Tab>'] = { 'snippet_forward', 'fallback' },
+      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+      ['<C-k>'] = { 'select_prev', 'fallback' },
+      ['<C-j>'] = { 'select_next', 'fallback' },
+      ['<C-p>'] = { 'select_prev', 'fallback' },
+      ['<C-n>'] = { 'select_next', 'fallback' },
+
+      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+    },
+    completion = {
+    list = {
+        selection = {
+          -- disable auto selection for cmd
+          preselect = function(ctx) return ctx.mode ~= 'cmdline' end,
+          auto_insert = function(ctx) return ctx.mode ~= 'cmdline' end
+        }
+      }
+    },
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono'
