de2ff10496be4d4975e6a9095e5ea07a1fdeeab8
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-01-19 15:01:48 +0100

    added oil

---
+++ .config/nvim/lua/config/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    opts = {
+      view_options = {
+        show_hidden = false,
+      }
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function ()
+      require("oil").setup()
+    end
+  }
+}
