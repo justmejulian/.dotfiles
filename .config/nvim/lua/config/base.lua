db30558b7ec0db0c120dfc6d9c2bf25b95b6fe87
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-01-19 12:50:55 +0100

    added my base changes

---
+++ .config/nvim/lua/config/base.lua
@@ -0,0 +1,51 @@
+print('hello from base.lua')
+local home_dir = "/Users/julian.visser"
+
+-- Set node version to use for LSP
+-- test using :!node -v
+local node_bin =  "/.nvm/versions/node/v20.18.0/bin"
+vim.g.node_host_prog = home_dir .. node_bin .. "/node"
+
+vim.cmd("let $PATH = '" .. home_dir .. node_bin .. ":' . $PATH")
+
+vim.g.mapleader = " "
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.opt.spelllang = 'en_gb'
+vim.opt.spell = true
+vim.opt.spelloptions = "camel"
+
+vim.opt.laststatus = 2
+
+vim.opt.mouse = vim.opt.mouse + 'a'
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.list = true
+--[[
+vim.opt.listchars = {
+    nbsp = '␣',
+    trail = '•',
+    eol = '↵',
+    tab = '--→',
+}
+]]
+--
+
+vim.opt.listchars = {
+  nbsp = '␣',
+  trail = '•',
+  tab = '--→',
+}
