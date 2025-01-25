6ee532d62ee14edd3eedc0628ce04ccbd05570c6
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-01-25 17:44:35 +0100

    fix rename

--- .config/nvim/lua/config/plugins/inc-rename.lua
+++ .config/nvim/lua/config/plugins/inc-rename.lua
@@ -1,13 +1,9 @@
 return {
   "smjonas/inc-rename.nvim",
-  opts = {},
-  keys = {
-    {
-      "<leader>rn",
-      function()
-        return ":IncRename " .. vim.fn.expand("<cword>")
-      end,
-      desc = "rn"
-    },
-  },
+  config = function()
+    requi("inc_rename").setup({});
+    vim.keymap.set("n", "<leader>rn", function()
+      return ":IncRename " .. vim.fn.expand("<cword>")
+    end, { expr = true })
+  end
 }
