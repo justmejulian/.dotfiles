81422650a8eab20bcc0fc43d9cf91fdd2e824f2b
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-01-28 21:23:31 +0100

    lsp fixes

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -9,7 +9,8 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { "lua_ls", "bashls", "vtsls" },
+      -- todo: move this onto lsp lang imports
+      ensure_installed = { "lua_ls", "bashls", "vtsls", "tinymist" },
     }
   },
   {
@@ -28,7 +29,16 @@ return {
       local lspconfig = require("lspconfig")
       require('config.plugins.lsp.lua').setup(lspconfig, capabilities)
       require('config.plugins.lsp.bash').setup(lspconfig, capabilities)
+      require('config.plugins.lsp.typst').setup(lspconfig, capabilities)
     end,
-    import = "config.plugins.lsp.typescript"
+    import = "config.plugins.lsp.typescript",
+    keys = {
+      { 'gD',         vim.lsp.buf.declaration,                                     desc = "lsp declaration" },
+      { 'gd',         function() vim.lsp.buf.definition { on_list = on_list } end, desc = "lsp definition" },
+      { 'K',          vim.lsp.buf.hover,                                           desc = "lsp hover" },
+      { '<C-k>',      vim.lsp.buf.signature_help,                                  desc = "lsp signature_help" },
+      { '<leader>ca', vim.lsp.buf.code_action,                                     desc = "lsp code_action" },
+      { '<leader>f',  function() vim.lsp.buf.format { async = true } end,          desc = "lsp format" },
+    }
   }
 }
--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -2,36 +2,42 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
-        'regex',
-        'markdown',
-        'markdown_inline',
-        'bash',
-        'astro',
-        "tsx",
-        'typescript',
-        "php",
-        "json",
-        "yaml",
-        "css",
-        "html",
-        "python",
-        "vimdoc",
-        "lua"
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<Leader>v",
-          node_incremental = "<Leader>v",
-          scope_incremental = false,
-          node_decremental = "<Leader>V",
+    lazy = false,
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        -- todo: auto add by language
+        ensure_installed = {
+          'regex',
+          'markdown',
+          'markdown_inline',
+          'bash',
+          'astro',
+          "tsx",
+          'typescript',
+          "php",
+          "json",
+          "yaml",
+          "css",
+          "html",
+          "python",
+          "vimdoc",
+          "lua",
+          "typst"
         },
-      },
-    }
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+        -- todo: does this work?
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<leader>v",
+            node_incremental = "<leader>v",
+            scope_incremental = false,
+            node_decremental = "<leader>V",
+          },
+        },
+      })
+    end
   }
 }
