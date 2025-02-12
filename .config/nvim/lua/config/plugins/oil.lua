f33484c13df8a15306f14226a0fe09593d4d5850
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-02-12 13:23:35 +0100

    allow for moveing between windows

--- .config/nvim/lua/config/plugins/oil.lua
+++ .config/nvim/lua/config/plugins/oil.lua
@@ -25,6 +25,7 @@ return {
     keymaps = {
       ["<CR>"] = { "actions.select", opts = { close = true } },
       ["<C-v>"] = { "actions.select", opts = { vertical = true, close = true } },
+      ["<C-h>"] = false,
     },
     float = {
       -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
