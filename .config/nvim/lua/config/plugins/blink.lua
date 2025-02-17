145a444c0f1c0ff259323b082d4e127ad73a68c5
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-02-17 08:32:53 +0100

    added codecompanion

--- .config/nvim/lua/config/plugins/blink.lua
+++ .config/nvim/lua/config/plugins/blink.lua
@@ -1,53 +1,57 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = 'rafamadriz/friendly-snippets',
-  version = 'v0.*',
-  opts = {
-    keymap = {
-      preset = 'none',
+  {
+    "olimorris/codecompanion.nvim",
+  },
+  {
+    'saghen/blink.cmp',
+    dependencies = 'rafamadriz/friendly-snippets',
+    version = 'v0.*',
+    opts = {
+      keymap = {
+        preset = 'none',
 
-      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-      ['<C-e>'] = { 'hide', 'fallback' },
-      ['<CR>'] = { 'accept', 'fallback' },
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
 
-      ['<Tab>'] = { 'snippet_forward', 'fallback' },
-      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
 
-      ['<C-k>'] = { 'select_prev', 'fallback' },
-      ['<C-j>'] = { 'select_next', 'fallback' },
-      ['<C-p>'] = { 'select_prev', 'fallback' },
-      ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<C-k>'] = { 'select_prev', 'fallback' },
+        ['<C-j>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
 
-      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-    },
-    signature = { enabled = true },
-    completion = {
-      list = {
-        selection = {
-          -- disable auto selection for cmd
-          preselect = function(ctx) return ctx.mode ~= 'cmdline' end,
-          auto_insert = function(ctx) return ctx.mode ~= 'cmdline' end
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      },
+      signature = { enabled = true },
+      completion = {
+        list = {
+          selection = {
+            -- disable auto selection for cmd
+            preselect = function(ctx) return ctx.mode ~= 'cmdline' end,
+            auto_insert = function(ctx) return ctx.mode ~= 'cmdline' end
+          }
         }
-      }
-    },
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono'
-    },
-    sources = {
-      -- todo: add luasnip https://github.com/Saghen/blink.cmp/blob/main/README.md#luasnip
-      default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-      providers = {
-        lazydev = {
-          name = "LazyDev",
-          module = "lazydev.integrations.blink",
-          -- make lazydev completions top priority (see `:h blink.cmp`)
-          score_offset = 100,
+      },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono'
+      },
+      sources = {
+        -- todo: add luasnip https://github.com/Saghen/blink.cmp/blob/main/README.md#luasnip
+        default = { "lazydev", "lsp", "path", "snippets", "buffer", "codecompanion" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
         },
       },
-
     },
-  },
-  opts_extend = { "sources.default" }
+    opts_extend = { "sources.default" }
+  }
 }
---
+++ .config/nvim/lua/config/plugins/codecompanion.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    -- replace with https://github.com/zbirenbaum/copilot.lua
+    "github/copilot.vim"
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "copilot",
+        },
+        inline = {
+          adapter = "copilot",
+        },
+        agent = {
+          adapter = "copilot",
+        },
+      },
+    }
+  }
+}
