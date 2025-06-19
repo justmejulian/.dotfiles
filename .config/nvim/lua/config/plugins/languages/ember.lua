ab7bddc99403001c126bf58806a579adefc789e2
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-06-19 16:10:12 +0200

    ember format everyhting with prettier

--- .config/nvim/lua/config/plugins/languages/ember.lua
+++ .config/nvim/lua/config/plugins/languages/ember.lua
@@ -38,7 +38,16 @@ end
 return {
   {
     'stevearc/conform.nvim',
-    opts = { formatters_by_ft = { javascript = { 'prettierd' } } },
+    opts = {
+      formatters_by_ft = {
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
+        html = { 'prettierd' },
+        css = { 'prettierd' },
+        glimmer = { 'prettierd' },
+        handlebars = { 'prettierd' },
+      },
+    },
   },
   {
     'nvim-treesitter/nvim-treesitter',
