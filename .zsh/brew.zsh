49c558283ebc35330e751647842052fa39376119
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-10-09 13:32:47 +0200

    Added brew update all command

--- .zsh/brew.zsh
+++ .zsh/brew.zsh
@@ -1,5 +1,4 @@
 function brew() {
-
   local dump_commands=('install' 'uninstall') # Include all commands that should do a brew dump
   local main_command="${1}"
 
@@ -8,9 +7,26 @@ function brew() {
   for command in "${dump_commands[@]}"; do
     if [[ "${command}" == "${main_command}" ]]
     then
-	    echo ' '
+      echo ' '
       echo 'Updating .Brewfile'
       /opt/homebrew/bin/brew bundle dump --file="${HOME}/.Brewfile" --force
     fi
   done
+}
+
+alias bu='homebrew_update_all'
+
+function homebrew_update_all() {
+  echo ' '
+  echo 'Updating Homebrew...'
+  /opt/homebrew/bin/brew update
+  echo ' '
+  echo 'Upgrading Homebrew packages...'
+  /opt/homebrew/bin/brew upgrade
+  echo ' '
+  echo 'Upgrading Homebrew casks packages...'
+  /opt/homebrew/bin/brew upgrade --cask $(brew list --cask)
+  echo ' '
+  echo 'Cleaning up Homebrew...'
+  /opt/homebrew/bin/brew cleanup
 }
