a049cf990f2afc9efa7e9d9a39fbe7cceff8d0f0
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-11-11 09:43:00 +0100

    improve git commands

--- .config/nvim/lua/config/plugins/diffview.lua
+++ .config/nvim/lua/config/plugins/diffview.lua
@@ -1,10 +1,15 @@
 return {
   'sindrets/diffview.nvim',
+  lazy = false,
   opts = {
     view = {
       merge_tool = {
         layout = 'diff3_mixed',
       },
     },
+  },
+  keys = {
+    { '<leader>gdo', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iffview [O]pen' },
+    { '<leader>gdc', '<cmd>DiffviewClose<cr>', desc = '[G]it [D]iffview [C]lose' },
   },
 }
--- .config/nvim/lua/config/plugins/gitsigns.lua
+++ .config/nvim/lua/config/plugins/gitsigns.lua
@@ -11,9 +11,11 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
     },
   },
   keys = {
-    { '<leader>gb', '<cmd>Gitsigns blame_line<cr>', desc = 'Toggle Git Blame' },
-    { '<leader>gn', '<cmd>Gitsigns next_hunk<cr>', desc = 'Git Next Change' },
-    { '<leader>gN', '<cmd>Gitsigns prev_hunk<cr>', desc = 'Git Previous Change' },
-    { '<leader>gp', '<cmd>Gitsigns preview_hunk_inline<cr>', desc = 'Git Previous Change' },
+    { '<leader>gb', '<cmd>Gitsigns blame_line<cr>', desc = '[G]it [B]lame' },
+    { '<leader>gn', '<cmd>Gitsigns next_hunk<cr>', desc = '[G]it [N]ext Change' },
+    { '<leader>gp', '<cmd>Gitsigns prev_hunk<cr>', desc = '[G]it [P]revious Change' },
+    { '<leader>ghp', '<cmd>Gitsigns preview_hunk_inline<cr>', desc = '[G]it [H]unk [P]review Inline' },
+    { '<leader>ghs', '<cmd>Gitsigns stage_hunk<cr>', desc = '[G]it [H]unk [S]tage' },
+    { '<leader>ghr', '<cmd>Gitsigns reset_hunk<cr>', desc = '[G]it [H]unk [R]eset' },
   },
 }
